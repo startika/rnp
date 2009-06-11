85f755ec7d99838f43162bd339254b1099725eb7
Roman Smirnov <POMAHC@gmail.com>
2009-06-11 12:38:08 +0400

    Improved Podcast model spec with shoulda rspec-matchers

--- spec/models/podcast_spec.rb
+++ spec/models/podcast_spec.rb
@@ -2,21 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Podcast do
 
-  it "should validate presence of title" do
-    podcast = Factory.build(:podcast, :title => "")
-    podcast.should_not be_valid
-
-    podcast = Factory.build(:podcast, :title => nil)
-    podcast.should_not be_valid
-  end
-
-  it "should validate presence of asset_link" do
-    podcast = Factory.build(:podcast, :asset_link => "")
-    podcast.should_not be_valid
-
-    podcast = Factory.build(:podcast, :asset_link => nil)
-    podcast.should_not be_valid
-  end
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:asset_link) }
 
   describe "performing operations requiring its number to be updated" do
 
